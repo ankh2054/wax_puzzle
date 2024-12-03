bd4a81fbb15bf31cc53e0b23f16ffb7f4556562d
Charles Holtzkampf <charles.holtzkampf@gmail.com>
2024-12-03 10:44:32 +0000

    contract update

--- contracts/gamecontract.hpp
+++ contracts/gamecontract.hpp
@@ -1,7 +1,6 @@
 #pragma once
 #include <eosio/eosio.hpp>
 #include <eosio/asset.hpp>
-#include <eosio/print.hpp>
 
 using namespace eosio;
 
@@ -9,11 +8,6 @@ class [[eosio::contract("gamecontract")]] gamecontract : public contract {
 public:
     using contract::contract;
 
-    // Constructor
-    gamecontract(name receiver, name code, datastream<const char*> ds)
-        : contract(receiver, code, ds) {}
-
-    // Public actions
     [[eosio::action]]
     void validateqfee(name user, asset fee);
 
@@ -33,26 +27,23 @@ public:
     void removegame(name user);
 
 private:
-    // Constants
     const asset MIN_FEE = asset(100000000, symbol("WAX", 8));  // 1.00000000 WAX
 
-    // Game table
     struct [[eosio::table]] game {
-        name user;          // Player's WAX account
-        bool challenge1;    // Challenge 1 status
-        bool challenge2;    // Challenge 2 status
-        bool challenge3;    // Challenge 3 status
-        uint32_t game_entries;  // Number of paid entries (increments with each 1 WAX payment)
-        uint32_t entries_used;  // Number of entries used
+        name user;
+        bool challenge1;
+        bool challenge2;
+        bool challenge3;
+        uint32_t game_entries;
+        uint32_t entries_used;
 
         uint64_t primary_key() const { return user.value; }
     };
     typedef multi_index<"games"_n, game> game_table;
 
-    // Prize pool table
     struct [[eosio::table]] prize_pool {
-        uint64_t id;        // Singleton ID (always 0)
-        asset amount;       // Prize pool amount
+        uint64_t id;
+        asset amount;
 
         uint64_t primary_key() const { return id; }
     };
